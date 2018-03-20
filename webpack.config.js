b89d2c88cd1060b5430c0aa01e90054a0d595253
dabb1988 <57229509@qq.com>
2018-03-20 17:40:53 +0800

    liubo

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
     mode: "development",
-    // ...
+    entry: {
+    	app: './src/index.js'
+    },
+    output: {
+    	filename: '[name].js',
+    	publicPath: './dist',
+    	path: path.resolve(__dirname, './dist')
+    }
 }
